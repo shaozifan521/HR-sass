c887aed9f2de2863b71186675dcd7ba95c1ba515
邵宏博 <137164891@qq.com>
2021-11-30 17:10:32 +0800

    封装Vuex的登录Action并处理token

--- src/store/getters.js
+++ src/store/getters.js
@@ -1,8 +1,13 @@
+/*
+ * @Author: shaohb
+ * @Date: 2021-11-25 16:59:00
+ * @LastEditors: shaohb
+ * @LastEditTime: 2021-11-30 17:09:47
+ * @Description:
+ */
 const getters = {
   sidebar: state => state.app.sidebar,
   device: state => state.app.device,
-  token: state => state.user.token,
-  avatar: state => state.user.avatar,
-  name: state => state.user.name
+  token: state => state.user.token // 在根级的getters上 开发子模块的属性给别人看 给别人用
 }
 export default getters
--- src/store/modules/user.js
+++ src/store/modules/user.js
@@ -1,95 +1,36 @@
-import { login, logout, getInfo } from '@/api/user'
+import { login } from '@/api/user'
 import { getToken, setToken, removeToken } from '@/utils/auth'
-import { resetRouter } from '@/router'
 
-const getDefaultState = () => {
-  return {
-    token: getToken(),
-    name: '',
-    avatar: ''
-  }
+const state = {
+  // 初始化的时候从缓存中读取状态 并赋值到初始化的状态上
+  token: getToken() // 设置token初始状态   token持久化 => 放到缓存中
 }
 
-const state = getDefaultState()
-
 const mutations = {
-  RESET_STATE: (state) => {
-    Object.assign(state, getDefaultState())
+  // 设置token
+  setToken(state, token) {
+    state.token = token // 设置token
+    setToken(token) // vuex和 缓存数据的同步
   },
-  SET_TOKEN: (state, token) => {
-    state.token = token
-  },
-  SET_NAME: (state, name) => {
-    state.name = name
-  },
-  SET_AVATAR: (state, avatar) => {
-    state.avatar = avatar
+  // 删除缓存
+  removeToken(state) {
+    state.token = null // 删除vuex的token
+    removeToken() // 先清除 vuex  再清除缓存 vuex和 缓存数据的同步
   }
 }
 
 const actions = {
-  // user login
-  login({ commit }, userInfo) {
-    const { username, password } = userInfo
-    return new Promise((resolve, reject) => {
-      login({ username: username.trim(), password: password }).then(response => {
-        const { data } = response
-        commit('SET_TOKEN', data.token)
-        setToken(data.token)
-        resolve()
-      }).catch(error => {
-        reject(error)
-      })
-    })
-  },
-
-  // get user info
-  getInfo({ commit, state }) {
-    return new Promise((resolve, reject) => {
-      getInfo(state.token).then(response => {
-        const { data } = response
-
-        if (!data) {
-          return reject('Verification failed, please Login again.')
-        }
-
-        const { name, avatar } = data
-
-        commit('SET_NAME', name)
-        commit('SET_AVATAR', avatar)
-        resolve(data)
-      }).catch(error => {
-        reject(error)
-      })
-    })
-  },
-
-  // user logout
-  logout({ commit, state }) {
-    return new Promise((resolve, reject) => {
-      logout(state.token).then(() => {
-        removeToken() // must remove  token  first
-        resetRouter()
-        commit('RESET_STATE')
-        resolve()
-      }).catch(error => {
-        reject(error)
-      })
-    })
-  },
-
-  // remove token
-  resetToken({ commit }) {
-    return new Promise(resolve => {
-      removeToken() // must remove  token  first
-      commit('RESET_STATE')
-      resolve()
-    })
+  async login(context, data) {
+    const result = await login(data)
+    // axios默认给数据加了一层data
+    if (result.data.success) {
+      // actions 修改state 必须通过mutations
+      context.commit('setToken', result.data.data)
+    }
   }
 }
 
 export default {
-  namespaced: true,
   state,
   mutations,
   actions
